caa4dfeeb1093c4cf1451d086f14f00e43a11647
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-05 18:45:09 +0800

    Add TextArea to MainView and update save button action to print text

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -3,13 +3,17 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class MainController {
     @FXML
     private Button button;
+    @FXML
+    private TextArea mainTextArea;
 
     @FXML
     public void saveButtonAction(ActionEvent event) {
-        System.out.println("File has been saved...");
+        String temp = mainTextArea.getText();
+        System.out.println(temp);
     }
 }
