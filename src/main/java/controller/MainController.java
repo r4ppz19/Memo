0ee08182e396359b9c7bd9f8ff6f599fb057b3de
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-05 19:02:58 +0800

    Add file saving functionality to MainController and update .gitignore

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,9 +15,24 @@ public class MainController {
     @FXML
     private TextArea mainTextArea;
 
+    boolean oneTry = true;
+
+
     @FXML
-    public void saveButtonAction(ActionEvent event) {
-        String temp = mainTextArea.getText();
-        System.out.println(temp);
+    public void saveButtonAction(ActionEvent event) throws IOException{
+        saveFile();
+    }
+
+    // Save file method
+    private void saveFile() throws IOException{
+        if (oneTry) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"));
+            writer.write(mainTextArea.getText());
+            writer.close();
+            mainTextArea.clear();
+            oneTry = false;
+        } else {
+            System.out.println("FUCK! IT DID NOT WORK");
+        }
     }
 }
