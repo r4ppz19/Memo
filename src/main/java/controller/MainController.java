4d79c7ba352459989f495c48afc1849a6752d9cc
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-05 18:23:46 +0800

    Implement save button functionality and update MainView layout

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,5 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainController {
-    
+    @FXML
+    private Button button;
+
+    @FXML
+    public void saveButtonAction(ActionEvent event) {
+        System.out.println("File has been saved...");
+    }
 }
