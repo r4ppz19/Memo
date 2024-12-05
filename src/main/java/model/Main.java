0e975c19f3207a3949f01c16cb76f9f705c35ccd
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-05 18:15:52 +0800

    Add initial project structure with Maven configuration and JavaFX setup

---
+++ src/main/java/model/Main.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.Objects;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainView.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
